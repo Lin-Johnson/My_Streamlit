bf1fcccd27b8796b5a3b83a64ae5226e0a00c34a
Lin-Johnson <104552255+Lin-Johnson@users.noreply.github.com>
2023-03-23 15:57:28 +0800

    Update 1.py

--- First_Demo/1.py
+++ First_Demo/1.py
@@ -7,7 +7,7 @@ import os
 progress_bar = st.sidebar.progress(0)
 status_text = st.sidebar.empty()
 
-df = pd.read_excel("First_Demo/Streamlit_Demo.xlsx")
+df = pd.read_excel("Streamlit_Demo.xlsx")
 chart_data = df.loc[:,['高温(℃)','低温(℃)']]
 chart_data.index = df['2022年月份']
 chart = st.line_chart(chart_data)
