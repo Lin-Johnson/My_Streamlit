7bb60e30a736d2cd36b1645900c3f64058305bc4
Lin-Johnson <104552255+Lin-Johnson@users.noreply.github.com>
2023-03-23 14:56:00 +0800

    Update 1.py

--- First_Demo/1.py
+++ First_Demo/1.py
@@ -7,15 +7,15 @@ import os
 path_to_find = os.listdir()
 st.title(path_to_find)
 
-progress_bar = st.sidebar.progress(0)
-status_text = st.sidebar.empty()
+# progress_bar = st.sidebar.progress(0)
+# status_text = st.sidebar.empty()
 
-df = pd.read_excel("https://github.com/Lin-Johnson/My_Streamlit/edit/main/First_Demo/Streamlit_Demo.xlsx")
-chart_data = df.loc[:,['高温(℃)','低温(℃)']]
-chart_data.index = df['2022年月份']
-chart = st.line_chart(chart_data)
+# df = pd.read_excel("https://github.com/Lin-Johnson/My_Streamlit/edit/main/First_Demo/Streamlit_Demo.xlsx")
+# chart_data = df.loc[:,['高温(℃)','低温(℃)']]
+# chart_data.index = df['2022年月份']
+# chart = st.line_chart(chart_data)
 
-progress_bar.empty()
+# progress_bar.empty()
 
 
 # Streamlit widgets automatically run the script from top to bottom. Since
